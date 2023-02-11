98b509924acc2166b77355c83cf46b74e01d30b4
kennethpole6 <polekenneth11@gmail.com>
2023-02-11 15:31:26 +0800

    ui changed, favicon added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      animation: {
-        cursor: "cursor .6s linear infinite alternate",
-        type: "type 1.8s ease-out .8s 1 normal both",
-        "type-reverse": "type 1.8s ease-out 0s infinite alternate-reverse both",
-      },
-      keyframes: {
-        type: {
-          "0%": { width: "0ch" },
-          "25%, 30%": { width: "3ch" },
-          "35%, 40%": { width: "4ch" },
-          "65%, 70%": { width: "7ch" },
-          "75%, 80%": { width: "8ch" },
-          "95%": { width: "10ch" },
-        },
+      fontFamily: {
+        primary: ["Sora"],
       },
     },
   },

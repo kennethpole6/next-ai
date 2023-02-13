96b329bb23eab10c8b6809d52afc8cbacbef9558
kennethpole6 <polekenneth11@gmail.com>
2023-02-13 16:42:22 +0800

    edge functions added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,10 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "@next/font/google";
 
+export const config = {
+  runtime: "edge",
+};
+
 const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
